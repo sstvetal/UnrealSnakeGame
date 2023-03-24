c11435677676a0f65242321ab62ae3c24a45b048
Vitalik <sstvetal@gmail.com>
2023-03-25 00:19:59 +0200

    lesson 1 - tests

---
+++ Source/SnakeGame/Tests/Framework.spec.cpp
@@ -0,0 +1,37 @@
+// Snake Game, Copyright LifeEXE. All Rights Reserved.
+
+#if WITH_AUTOMATION_TESTS
+
+#include "CoreMinimal.h"
+#include "Misc/AutomationTest.h"
+#include "SnakeGame/Tests/Utils/TestUtils.h"
+#include "SnakeGame/Framework/SG_GameMode.h"
+#include "SnakeGame/Framework/SG_Pawn.h"
+
+BEGIN_DEFINE_SPEC(FSnakeFramework, "Snake",
+    EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority)
+UWorld* World;
+END_DEFINE_SPEC(FSnakeFramework)
+
+void FSnakeFramework::Define()
+{
+	using namespace LifeExe::Test;
+    Describe("Framework", [this]()
+		 { 
+			 BeforeEach([this]()
+						{
+							AutomationOpenMap("GameLevel");
+                            World = GetTestGameWorld();
+						});
+            It("GameMapmightExist", [this]() { TestNotNull("World Exists", World); }); 
+			It("ClassesMightBeSetupCorrectly", 
+			   [this]() 
+			   { 
+				   TestNotNull("Snake game mode set up ", Cast<ASG_GameMode>(World->GetAuthGameMode())); 
+			       TestNotNull("Snake pawn mode set up ", Cast<ASG_Pawn>(World->GetFirstPlayerController()->GetPawn()));
+			   }); 
+			xIt("PawnLocationShouldBeAdjastCorrectly", [this]() { unimplemented(); }); 
+		 });
+}
+
+#endif
---
+++ Source/SnakeGame/Tests/Grid.spec.cpp
@@ -0,0 +1,25 @@
+// Snake Game, Copyright LifeEXE. All Rights Reserved.
+
+#if WITH_AUTOMATION_TESTS
+
+#include "CoreMinimal.h"
+#include "Misc/AutomationTest.h"
+#include "SnakeGame/Core/Grid.h"
+
+DEFINE_SPEC(FSnakeGrid, "Snake", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority)
+
+void FSnakeGrid::Define()
+{
+	using namespace Snake;
+    Describe("CoreGrid", [this]() {//
+	     It("DimsMightIncludeWalls", 
+		 [this]() 
+		 { 
+			    const Grid grid(Dim{12, 10});
+                TestTrueExpr(grid.dim().height == 14);
+                TestTrueExpr(grid.dim().width == 12);
+		 }); 
+	});
+}
+
+#endif
