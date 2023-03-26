b9fb891b86298e87374b06308fc9da19e1faf2ba
Vitalik <sstvetal@gmail.com>
2023-03-26 18:19:50 +0300

    lesson 1 - fix clang and docs workflow

--- Source/SnakeGame/SnakeGame.Build.cs
+++ Source/SnakeGame/SnakeGame.Build.cs
@@ -4,16 +4,16 @@ using UnrealBuildTool;
 
 public class SnakeGame : ModuleRules
 {
-	public SnakeGame(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public SnakeGame(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
-		PublicIncludePaths.AddRange(new string[] { "SnakeGame" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+        PublicIncludePaths.AddRange(new string[] { "SnakeGame" });
 
-		if (Target.Configuration != UnrealTargetConfiguration.Shipping)
-		{
-			PublicDependencyModuleNames.Add("FunctionalTesting");
+        if (Target.Configuration != UnrealTargetConfiguration.Shipping)
+        {
+            PublicDependencyModuleNames.Add("FunctionalTesting");
         }
-	}
+    }
 }
